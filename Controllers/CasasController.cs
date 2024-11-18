3f1bfa3ed627fe360fb0b953acb9a07c44dfd312
Alexandra Segura <asegurarodriguez@outlook.com>
2024-11-18 17:03:28 -0600

    Se agrega boton de Inactivar casa con su respectivo metodo y mensaje al usuario

--- Controllers/CasasController.cs
+++ Controllers/CasasController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
 using static DataModels.PviProyectoFinalDBStoredProcedures;
 
 namespace G03_Sistema_Condominios.Controllers
@@ -28,21 +29,38 @@ namespace G03_Sistema_Condominios.Controllers
         {
             var casa = new ModelCasa();
 
-            using (var db = new PviProyectoFinalDB("MyDatabase"))
+           try
             {
-                casa = db.SpConsultarCasasPorID(idCasa).Select(x => new ModelCasa
+                using (var db = new PviProyectoFinalDB("MyDatabase"))
                 {
-                    IdCasa = x.Id_casa,
-                    NombreCasa = x.Nombre_casa,
-                    MetrosCuadrados = x.Metros_cuadrados,
-                    NumeroHabitaciones = x.Numero_habitaciones,
-                    NumeroBanos = x.Numero_banos,   
-                    IdPersona = x.Id_persona,
-                    FechaConstruccion = x.Fecha_construccion
-                }).FirstOrDefault();
-                
-                //puede que sea necesario modificar este sp o bien crear el correspondiente para tener acceso a las personas
-                ViewBag.Clientes = db.SpConsultarClientesActivos().ToList();
+                    casa = db.SpConsultarCasasPorID(idCasa).Select(x => new ModelCasa
+                    {
+                        IdCasa = x.Id_casa,
+                        NombreCasa = x.Nombre_casa,
+                        MetrosCuadrados = x.Metros_cuadrados,
+                        NumeroHabitaciones = x.Numero_habitaciones,
+                        NumeroBanos = x.Numero_banos,
+                        IdPersona = x.Id_persona,
+                        FechaConstruccion = x.Fecha_construccion,
+                        Estado = (bool)x.Estado
+                    }).FirstOrDefault();
+
+                    //puede que sea necesario modificar este sp o bien crear el correspondiente para tener acceso a las personas
+                    ViewBag.Clientes = db.SpConsultarClientesActivos().ToList();
+
+                    // Si el servicio está inactivo, muestra un mensaje o redirige a otra página
+                    if (casa != null && !casa.Estado)
+                    {
+                        TempData["Resultado"] = "La casa seleccionada está inactiva y no puede ser modificada.";
+                        return RedirectToAction("Index"); // Redirige a Index
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Resultado"] = "Ocurrió un error al cargar el la casa: " + ex.Message;
+                return RedirectToAction("Index");
             }
 
             return View(casa);
@@ -76,6 +94,26 @@ namespace G03_Sistema_Condominios.Controllers
             }
 
             return View();
+        }
+
+        public ActionResult InactivarCasa(int? idCasa)
+        {
+            using (var db = new PviProyectoFinalDB("MyDatabase"))
+            {
+                //verificar el estado de la casa 
+               // var casa = db.SpConsultarCasasPorID(idCasa).FirstOrDefault();
+
+                //if (casa != null && !casa.Estado)
+                //{
+                //    ViewBag.Resultado = "La casa ya está inactiva.";
+                //    return RedirectToAction("Index");
+                //}              
+
+                db.SpInactivarCasa(idCasa);
+            }
+
+            return RedirectToAction("Index");
+
         }
     }
 }
