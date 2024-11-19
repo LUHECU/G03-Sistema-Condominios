1a5222d1d23a9cfd09c6bcf8b6d74254dbd1733c
Mefibooset <mrojas@cidgallup.com>
2024-11-19 17:33:00 -0600

    Primera parte

---
+++ Controllers/CobroController.cs
@@ -0,0 +1,30 @@
+﻿using DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static DataModels.PviProyectoFinalDBStoredProcedures;
+
+namespace G03_Sistema_Condominios.Controllers
+{
+    public class CobroController : Controller
+    {
+        // GET: Cobro
+        public ActionResult Index()
+        {
+            var list = new List<SpConsultarCobrosResult>();
+            try
+            {
+                using (var db = new PviProyectoFinalDB("MyDatabase"))
+                {
+                    list = db.SpConsultarCobros().ToList();
+
+                }
+            }
+            catch { }
+            return View(list);
+
+        }
+    }
+}
